6d05b8b8d0af4db9c90b4bfa76e7a07959152397
feiwenbo <2900195129@qq.com>
2026-01-27 21:23:10 +0800

    创建ship模块，显示飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
@@ -11,6 +12,7 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height)
         )
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
@@ -18,6 +20,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
             self.clock.tick(60)
 
